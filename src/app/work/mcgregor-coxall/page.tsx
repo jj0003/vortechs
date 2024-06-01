bd11cc57039f6451c50f1c53d4704604aa92a0e3
jan furio <janfurio@tutanota.com>
2024-06-01 20:29:27 +1000

    chore: Update page.tsx with new project images and links
    This commit updates the page.tsx file to include new project images and links for Peaches Pilates, Deus Ex Machina, and Ocean And Earth. The previous links were removed and replaced with the updated ones. This change improves the user experience by providing accurate and up-to-date information about the projects.

--- src/app/work/mcgregor-coxall/page.tsx
+++ src/app/work/mcgregor-coxall/page.tsx
@@ -99,36 +99,47 @@ export default function Home() {
       {/* What We Do */}
       <div className="bg-black rounded-md p-5 mt-20 lg:mx-20 mx-5  mb-20">
         <h1 className="my-10 satoshi-regular text-white text-center uppercase">More to explore</h1>
-
+        
       <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-5 mt-3">
       <div className="lg:h-96 h-48 flex flex-col justify-end">
-        <Link href={"/work"} className="relative group h-96 block rounded-lg overflow-hidden">
-          <div className="absolute inset-0 bg-[url('/images/McGregorCoxall.jpg')] bg-cover bg-center transition-all duration-300 group-hover:blur-sm"></div>
+        <Link href={"/work/peaches-pilates"} className="relative group h-96 block rounded-lg overflow-hidden">
+          <div className="absolute inset-0 bg-[url('/images/PeachesPilates.jpg')] bg-cover bg-center transition-all duration-300 group-hover:blur-sm"></div>
           <div className="absolute inset-0 flex items-center justify-center opacity-0 transition-opacity duration-300 group-hover:opacity-100">
-            <p className="text-white font-semibold text-lg">Your Revealed Text Here</p>
+            <p className="px-3 py-1 uppercase text-black text-xs rounded-full bg-white">
+              Explore
+            </p>
           </div>
         </Link>
         <h5 className="mt-2 text-white font-light text-sm">
-          McGregor Coxall
+          Peaches Pilates
         </h5>
       </div>
-
-        <div className="lg:h-96 h-48 flex flex-col justify-end">
-          <Link href={""} className="relative group h-96  block rounded-lg overflow-hidden">
-            <div className="absolute inset-0 bg-[url('/images/McGregorCoxall.jpg')] bg-cover bg-center transition-all duration-300 group-hover:blur-sm"></div>
-          </Link>
-          <h5 className="mt-2 text-white font-light text-sm">
-            McGregor Coxall
-          </h5>
-        </div>
-        <div className="lg:h-96 h-48 flex flex-col justify-end">
-          <Link href={""} className="relative group h-96  block rounded-lg overflow-hidden">
-            <div className="absolute inset-0 bg-[url('/images/McGregorCoxall.jpg')] bg-cover bg-center transition-all duration-300 group-hover:blur-sm"></div>
-          </Link>
-          <h5 className="mt-2 text-white font-light text-sm">
-            McGregor Coxall
-          </h5>
-        </div>
+      <div className="lg:h-96 h-48 flex flex-col justify-end">
+        <Link href={"/work/deus-ex-machina"} className="relative group h-96 block rounded-lg overflow-hidden">
+          <div className="absolute inset-0 bg-[url('/images/DeusExMachina.webp')] bg-cover bg-center transition-all duration-300 group-hover:blur-sm"></div>
+          <div className="absolute inset-0 flex items-center justify-center opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+            <p className="px-3 py-1 uppercase text-black text-xs rounded-full bg-white">
+              Explore
+            </p>
+          </div>
+        </Link>
+        <h5 className="mt-2 text-white font-light text-sm">
+          Deus Ex Machina
+        </h5>
+      </div>
+      <div className="lg:h-96 h-48 flex flex-col justify-end">
+        <Link href={"/work/peaches-pilates"} className="relative group h-96 block rounded-lg overflow-hidden">
+          <div className="absolute inset-0 bg-[url('/images/OceanAndEarth.webp')] bg-cover transition-all duration-300 group-hover:blur-sm"></div>
+          <div className="absolute inset-0 flex items-center justify-center opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+            <p className="px-3 py-1 uppercase text-black text-xs rounded-full bg-white">
+              Explore
+            </p>
+          </div>
+        </Link>
+        <h5 className="mt-2 text-white font-light text-sm">
+          Ocean And Earth
+        </h5>
+      </div>
       </div>
       </div>
 
