ca319c932a9e3fa3064d2fe97fae2dbb4fbcad17
jj0003 <janfuriocatalan@protonmail.com>
2025-01-14 19:45:37 +1100

    Fix typo in WallOfLove component review text

--- src/components/home/WallOfLove.tsx
+++ src/components/home/WallOfLove.tsx
@@ -23,7 +23,7 @@ const localReviews = [
     id: 4,
     name: "Zoe",
     title: "@Personal",
-    review: "Jan work ethic is unparalleled. He is creative, detail-oriented, and delivers beyond expectations!",
+    review: "Jan&apos;s work ethic is unparalleled. He is creative, detail-oriented, and delivers beyond expectations!",
   },
 ];
 
